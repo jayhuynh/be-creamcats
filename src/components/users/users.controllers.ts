55b951e409c47abd664007d487e0543db14630cd
camapcanhcut <khacduymath@gmail.com>
2021-10-01 09:14:11 +0700

    Update Profile

--- src/components/users/users.controllers.ts
+++ src/components/users/users.controllers.ts
@@ -1,10 +1,11 @@
 import { User } from "@prisma/client";
-import { Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import expressAsyncHandler from "express-async-handler";
 
 import { AuthorizedRequest } from "../../utils/express";
 import { prisma } from "../../utils";
-import { AuthError } from "../errors";
+import { AuthError, SchemaError, ConflictError } from "../errors";
+import Joi from "joi";
 
 export const getUserProfileOfMe = expressAsyncHandler(
   async (req: AuthorizedRequest, res: Response, next: NextFunction) => {
@@ -24,5 +25,55 @@ export const getUserProfileOfMe = expressAsyncHandler(
     } catch (e) {
       return next(e);
     }
+  }
+);
+
+export const updateUserProfile = expressAsyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const schema = Joi.object({
+      userId: Joi.number().integer().required(),
+      fullname: Joi.string(),
+      age: Joi.number().integer(),
+      profilePic: Joi.string(),
+    });
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+      return next(new SchemaError(error.message));
+    }
+
+    const { userId, fullname, age, profilePic } = value;
+
+    let existingUser: User;
+    try {
+      existingUser = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+    } catch (e) {
+      return next(e);
+    }
+    if (!existingUser) {
+      return next(new ConflictError("User with the same id does not exist"));
+    }
+
+    try {
+      await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          fullname: fullname,
+          age: age,
+          profilePic: profilePic,
+        },
+      });
+    } catch (e) {
+      return next(e);
+    }
+
+    return res.status(200).json({
+      message: "User Profile successfully updated",
+    });
   }
 );
--- src/components/users/users.routes.ts
+++ src/components/users/users.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authorizeUser } from "../auth";
-import { getUserProfileOfMe } from "./users.controllers";
+import { getUserProfileOfMe, updateUserProfile } from "./users.controllers";
 
 const router = express.Router();
 
+router.route("/me").post(updateUserProfile);
 router.route("/me").get(authorizeUser, getUserProfileOfMe);
 
 export { router };
