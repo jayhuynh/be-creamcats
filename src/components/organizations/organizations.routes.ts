ea806fc749911d7d2d2a6ab166c82e65e3d1f7f3
camapcanhcut <khacduymath@gmail.com>
2021-10-04 18:45:35 +0700

    Create Organization

--- src/components/organizations/organizations.routes.ts
+++ src/components/organizations/organizations.routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import { getOrganizationById } from "./organizations.controllers";
+import {
+  getOrganizationById,
+  createOrganization,
+} from "./organizations.controllers";
 import { router as eventsRouter } from "../../components/events";
 import { router as applicationRouter } from "../../components/applications";
 
 const router = express.Router();
 
+router.route("/").post(createOrganization);
 router.use("/:organizationId/events", eventsRouter);
 router.use("/:organizationId/applications", applicationRouter);
 
