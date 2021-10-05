fd5405344a06987815c5ac139cd79a5b9561cf28
nathan-wien <nhat.nguyen.cs17@gmail.com>
2021-10-05 20:39:21 +1000

    DEC-97 Adjust API to accept additional query params for filtering

--- sql/applicationsOfOrganization.sql
+++ sql/applicationsOfOrganization.sql
@@ -1,14 +1,14 @@
 SELECT
-  usr.id as user_id,
-  usr.fullname as user_fullname,
-  eve.id as event_id,
-  eve.name as event_name,
-  pos.id as position_id,
-  pos.name as position_name,
-  usr.gender as gender,
-  application.id as application_id,
-  application."timeCreated" as applied_at,
-  application.status as status
+  usr.id as "applicantId",
+  usr.fullname as "applicantName",
+  eve.id as "eventId",
+  eve.name as "eventName",
+  pos.id as "positionId",
+  pos.name as "positionName",
+  usr.gender as "gender",
+  application.id as "applicationId",
+  application."timeCreated" as "appliedAt",
+  application.status as "status"
 FROM
   "Application" as application
     JOIN(
@@ -42,51 +42,9 @@ FROM
         "Organization" as org
     ) AS org ON eve.org_id = org.id
 WHERE
-  org.id = 1;
-
-
--- SELECT *
--- FROM
---   "User" as u;
-
--- SELECT
---   pos.id as id,
---   pos.name as name,
---   pe.eve_id as "evendId",
---   pa.application_cnt as "applicationCount",
---   pe.coor <-> ST_MakePoint(152.99, -27.4942)::geography as distance
--- FROM
---   "Position" as pos
---     JOIN (
---       SELECT
---         pos.id as pos_id,
---         COUNT(*) AS application_cnt
---       FROM "Position" as pos, "Application" as app
---       WHERE pos.id = app."positionId"
---       GROUP BY pos.id
---     ) AS pa ON pos.id = pa.pos_id
---     JOIN (
---       SELECT
---         pos.id as pos_id,
---         eve."id" as eve_id,
---         eve."coor" as coor
---       FROM "Position" as pos, "Event" as eve
---       WHERE pos."eventId" = eve.id
---     ) AS pe ON pos.id = pe.pos_id
--- WHERE
---   pos.id IN (
---     SELECT pos.id as pos_id
---     FROM "Position" as pos, "_PositionToTag" as ptot, "Tag" as tag
---     WHERE pos.id = ptot."A"
---       AND ptot."B" = tag.id
---       AND (
---         tag.name = 'Community Services'
---       )
---     GROUP BY pos_id
---     HAVING COUNT(pos.id) > 0
---   )
---   AND pos.gender ILIKE 'male'
---   AND ST_DWITHIN(pe.coor, ST_MAKEPOINT(152.99, -27.4942), 20000)
--- -- ORDER BY "applicationCount" DESC
--- ORDER BY pe.coor <-> ST_MakePoint(152.99, -27.4942)::geography DESC
--- -- ORDER BY pos."timeCreated" DESC
+  org.id = 1
+  AND usr.gender = 'MALE'
+  AND (
+    (usr.fullname || eve.name || pos.name) ILIKE '%%'
+  )
+  ORDER BY "appliedAt" DESC;
