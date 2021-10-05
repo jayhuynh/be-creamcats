4b348553ddf0ab821e85afc03d3e2a7b71f821ea
camapcanhcut <khacduymath@gmail.com>
2021-10-05 13:49:32 +0700

    Remove duplicate API

--- src/components/organizations/organizations.controllers.ts
+++ src/components/organizations/organizations.controllers.ts
@@ -4,12 +4,7 @@ import expressAsyncHandler from "express-async-handler";
 import Joi from "joi";
 
 import { prisma } from "../../utils";
-import {
-  NotFoundError,
-  BadRequestError,
-  SchemaError,
-  ConflictError,
-} from "../errors";
+import { NotFoundError, BadRequestError } from "../errors";
 
 export const getOrganizationById = expressAsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -31,55 +26,5 @@ export const getOrganizationById = expressAsyncHandler(
     } catch (e) {
       return next(e);
     }
-  }
-);
-
-export const createOrganization = expressAsyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const schema = Joi.object({
-      email: Joi.string().email().required(),
-      name: Joi.string(),
-      desc: Joi.string(),
-      addr: Joi.string(),
-      phone: Joi.string(),
-    });
-    const { error, value } = schema.validate(req.body);
-    if (error) {
-      return next(new SchemaError(error.message));
-    }
-
-    const { email, name, desc, addr, phone } = value;
-
-    let existingOrganization: Organization;
-    try {
-      existingOrganization = await prisma.organization.findUnique({
-        where: { email },
-      });
-    } catch (e) {
-      return next(e);
-    }
-    if (existingOrganization) {
-      return next(
-        new ConflictError("Organization with the same email already exists")
-      );
-    }
-
-    try {
-      await prisma.organization.create({
-        data: {
-          email: email,
-          name: name,
-          desc: desc,
-          addr: addr,
-          phone: phone,
-        },
-      });
-    } catch (e) {
-      return next(e);
-    }
-
-    return res.status(200).json({
-      message: "Organization successfully created",
-    });
   }
 );
