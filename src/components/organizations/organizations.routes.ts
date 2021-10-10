3121bc7918d952d57d85ea1fa6234fe9762cba7b
nathan-wien <nhat.nguyen.cs17@gmail.com>
2021-10-10 18:05:14 +1000

    Bug fix for GET /organizations/me

--- src/components/organizations/organizations.routes.ts
+++ src/components/organizations/organizations.routes.ts
@@ -12,7 +12,7 @@ const router = express.Router();
 router.use("/:organizationId/events", eventsRouter);
 router.use("/:organizationId/applications", applicationRouter);
 
-router.route("/:id").get(getOrganizationById);
 router.route("/me").get(authorizeUser, getOrgProfile);
+router.route("/:id").get(getOrganizationById);
 
 export { router };
